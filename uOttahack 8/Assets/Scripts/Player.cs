a8350060115560f5d64e3107efba225a4c59b166
Akash-Kamran-Hoque <Akash.k.hoque@gmail.com>
2026-01-17 15:56:49 -0500

    Finished basic movement and jump

--- uOttahack 8/Assets/Scripts/Player.cs
+++ uOttahack 8/Assets/Scripts/Player.cs
@@ -4,8 +4,13 @@ public class Player : MonoBehaviour
 {
 
     public float movementSpeed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
 
     private Rigidbody2D rb;
+    private bool isGrounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,7 +20,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //Horizontal movement
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput*movementSpeed, rb.linearVelocity.y);
+
+        //Jumping (Verical movement)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    //Assuring that the player is on the ground
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 }
